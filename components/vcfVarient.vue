8af1ea237ace0dfd2829d4f3aa5bf2be9a19bb81
MrBrown6210 <bite6210@gmail.com>
2021-06-28 17:35:27 +0700

    doing jest

--- components/vcfVarient.vue
+++ components/vcfVarient.vue
@@ -5,7 +5,7 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
@@ -13,35 +13,59 @@ import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
 am4core.useTheme(am4themes_animated);
 
+interface varient {
+  type: string
+  original: number
+  filtered: number
+}
+
 export default defineComponent({
   setup() {
-    let varient;
+    let varient: any
     return { varient }
   },
   mounted() {
+    this.renderVarientChart()
   },
   methods: {
     renderVarientChart() {
-      const chart = am4core.create(this.$refs.varient, am4charts.XYChart)
+      console.log(this.$refs.varient)
+      let chart = am4core.create(this.$refs.varient as HTMLElement, am4charts.XYChart);
 
-      const category = new am4charts.CategoryAxis()
-      category.title.text = 'Variant'
-      category.dataFields.category = 'Local'
-      chart.xAxes.push(category)
+    chart.paddingRight = 20;
 
-
-
-      chart.data = [
-        {
-          x: 1,
-          y: 2,
-        },
-        {
-          x: 1,
-          y: 2,
-        }
-      ]
+    let data = [];
+    let visits = 10;
+    for (let i = 1; i < 5; i++) {
+      visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+      data.push({ date: new Date(2018, 0, i), name: "name" + i, value: visits });
     }
+
+    chart.data = data;
+
+    let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+    dateAxis.renderer.grid.template.location = 0;
+
+    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.tooltip!.disabled = true
+    valueAxis.renderer.minWidth = 35;
+
+    let series = chart.series.push(new am4charts.ColumnSeries());
+    series.dataFields.dateX = "date";
+    series.dataFields.valueY = "value";
+
+    series.tooltipText = "{valueY.value}";
+    chart.cursor = new am4charts.XYCursor();
+
+    // let scrollbarX = new am4charts.XYChartScrollbar();
+    // scrollbarX.series.push(series);
+    // chart.scrollbarX = scrollbarX;
+
+      this.varient = chart
+    }
+  },
+  beforeDestroy() {
+    this.varient?.dispose()
   }
 })
 </script>
