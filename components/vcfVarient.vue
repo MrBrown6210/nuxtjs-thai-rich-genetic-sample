c1a9a91432f31eaeea607ce260b102e15dff60bb
MrBrown6210 <bite6210@gmail.com>
2021-07-06 13:13:17 +0700

    Complete chaty

--- components/vcfVarient.vue
+++ components/vcfVarient.vue
@@ -1,68 +1,105 @@
 <template>
   <div>
-    <div>Varient Type</div>
-    <v-chart class="chart" :option="option" />
+    <p v-if="$fetchState.pending">Fetching mountains...</p>
+    <p v-else-if="$fetchState.error">An error occurred :(</p>
+    <div v-else>
+      <v-chart autoresize class="chart" :option="option" />
+    </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import VChart, { THEME_KEY } from "vue-echarts";
+import { defineComponent, onMounted, reactive, Ref, ref, useFetch } from '@nuxtjs/composition-api'
 
-export default {
+interface Varient {
+  type: string
+  original: number
+  filtered: number
+}
+
+export default defineComponent ({
   components: {
     VChart,
   },
   provide: {
-    [THEME_KEY]: "dark",
+    // [THEME_KEY]: "dark",
   },
-  data() {
-    return {
-      option: {
-        title: {
-          text: "Traffic Sources",
-          left: "center",
-        },
-        tooltip: {
-          trigger: "item",
-          formatter: "{a} <br/>{b} : {c} ({d}%)",
-        },
-        legend: {
-          orient: "vertical",
-          left: "left",
-          data: [
-            "Direct",
-            "Email",
-            "Ad Networks",
-            "Video Ads",
-            "Search Engines",
-          ],
-        },
-        series: [
-          {
-            name: "Traffic Sources",
-            type: "pie",
-            radius: "55%",
-            center: ["50%", "60%"],
-            data: [
-              { value: 335, name: "Direct" },
-              { value: 310, name: "Email" },
-              { value: 234, name: "Ad Networks" },
-              { value: 135, name: "Video Ads" },
-              { value: 1548, name: "Search Engines" },
-            ],
-            emphasis: {
-              itemStyle: {
-                shadowBlur: 10,
-                shadowOffsetX: 0,
-                shadowColor: "rgba(0, 0, 0, 0.5)",
-              },
-            },
-          },
-        ],
+  setup() {
+    const varients = ref([
+      {
+        type: 'test',
+        original: 200,
+        filtered: 300
+      }
+    ])
+    const option = {
+      title: {
+        text: "Varient Type",
+        left: "center",
       },
-    };
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: { type: 'shadow' }
+      },
+      series: [
+        {
+          data: [200, 200],
+          type: 'bar'
+        },
+        {
+          data: [200],
+          type: 'bar'
+        }
+      ],
+      xAxis: {
+        type: 'category',
+        // data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        data: ["test"]
+      },
+      yAxis: {
+          type: 'value',
+      },
+    }
+
+    const updateChart = () => {
+      console.log('update chart')
+      option.xAxis.data = []
+      option.series[0].data = []
+      option.series[1].data = []
+      varients.value.forEach(varient => {
+        option.xAxis.data.push(varient.type)
+        option.series[0].data.push(varient.original)
+        option.series[1].data.push(varient.filtered)
+      })
+    }
+
+    const { fetch, fetchState } = useFetch(() => {
+      console.log('fetch')
+      console.log(fetchState)
+      varients.value = [
+        {
+          type: 'SNPs',
+          original: Math.floor(Math.random() * 12000),
+          filtered: Math.floor(Math.random() * 12000),
+        },
+        {
+          type: 'detection',
+          original: Math.floor(Math.random() * 12000),
+          filtered: Math.floor(Math.random() * 12000),
+        },
+        {
+          type: 'insertion',
+          original: Math.floor(Math.random() * 12000),
+          filtered: Math.floor(Math.random() * 12000),
+        }
+      ]
+      updateChart()
+    })
+
+    return { option, varients, updateChart }
   },
-};
+});
 </script>
 
 <style scoped>
