49c753e721d0eae8c9c85d5906a2129ff36ac24b
MrBrown6210 <bite6210@gmail.com>
2021-07-06 10:29:50 +0700

    Add chart

--- plugins/echart.js
+++ plugins/echart.js
@@ -1,21 +1,27 @@
 import { use } from 'echarts/core'
 import { CanvasRenderer } from 'echarts/renderers'
-import { GraphChart } from 'echarts/charts'
-import { TitleComponent } from 'echarts/components'
-import { ToolboxComponent } from 'echarts/components'
-import { LegendComponent } from 'echarts/components'
-import { PieChart } from 'echarts/charts'
+import { GraphChart, PieChart, BarChart } from 'echarts/charts'
+import {
+  GridComponent,
+  TitleComponent,
+  ToolboxComponent,
+  TooltipComponent,
+  LegendComponent,
+} from 'echarts/components'
 
 import Vue from 'vue'
 import VChart from 'vue-echarts'
 
 use([
   CanvasRenderer,
+  GridComponent,
   GraphChart,
   TitleComponent,
+  TooltipComponent,
   ToolboxComponent,
   LegendComponent,
-  PieChart
+  PieChart,
+  BarChart
 ])
 
 Vue.use(VChart)
